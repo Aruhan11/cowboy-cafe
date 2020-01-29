4389b24a58879d80fd1e729e64a5be9807ff30bf
aruhan <aruhan@ksu.edu>
2020-01-29 15:09:24 -0600

    start

---
+++ Data/AngryChicken.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class AngryChicken
+    {
+        public double Price {
+            get { return 5.99; }
+        }
+
+        public uint Calories {
+            get { return 190; }
+        }
+
+        private bool pickle = true;
+
+        public bool Pickle {
+            get { return pickle; }
+            set { pickle = value; }
+        }
+    }
+}

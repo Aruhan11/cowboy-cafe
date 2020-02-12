68e175cd01ca5064fbc575d36105563e02a80696
aruhan <aruhan@ksu.edi>
2020-02-12 14:41:19 -0600

    modify

--- Data/Entree.cs
+++ Data/Entree.cs
@@ -4,12 +4,24 @@ using System.Text;
 
 namespace CowboyCafe.Data
 {
+    /// <summary>
+    /// A class  of base class of Entree
+    /// </summary>
     public abstract class Entree
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public abstract double Price { get; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public abstract uint Calories { get; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public abstract List<string> SpecialInstructions { get; }
     }
 }

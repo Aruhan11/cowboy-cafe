72474678e6ad61279a701a474a8546e4514c4676
aruhan <aruhan@ksu.edi>
2020-02-12 15:23:01 -0600

    in class

---
+++ Data/Drink.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public abstract class Drink
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public abstract double Price { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public abstract uint Calories { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual Size Size { get; set; } = Size.Small;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual bool Ice { get; set; } = true;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public abstract List<string> SpecialInstructions { get; }
+
+    }
+}
---
+++ Data/TexasTea.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class TexasTea: Drink
+    {
+        public Sweet RoomForCream { get; set; } = false;
+
+        public bool RoomForCream { get; set; } = false;
+
+    }
+}

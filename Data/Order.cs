0079061ca0fe20d2f4d4e74f16bb9d3d470caae1
aruhan <aruhan@ksu.edu>
2020-02-26 15:15:48 -0600

    in class

---
+++ Data/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace CowboyCafe.Data
+{
+    public class Order: INotifyPropertyChanged
+    {
+        private uint lastOrderNumber;
+
+        private List<IOrderItem> items;
+        
+        public IEnumerable<IOrderItem> Items => throw new NotImplementedException();
+
+        public double Subtotal { get; }
+
+        public uint OrderNumber { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Add(IOrderItem item) { }
+
+        public void Remove(IOrderItem item) { }
+
+    }
+}
